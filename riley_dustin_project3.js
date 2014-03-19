eb6df4d690f6460130d0d72bb999fc6d225423b9
Dustin Riley <driley137@fullsail.edu>
2014-03-18 22:20:58 -0400

    Getting some Thought process down.

--- riley_dustin_project3.js
+++ riley_dustin_project3.js
@@ -6,3 +6,31 @@
   Project 3
  */
 
+/*
+ * Pseudo
+ * ----------
+ * This program will prompt the user if they're ready to workout, this is the beginning of "motivation" and it becomes my boolean value. Once that is complete, I will proceed to get
+ * information through prompts and confirms. These include the time, name, how long they have until they have to be somewhere (instead of just limiting this to work hours).
+ *
+ * My JSON data will be in a seperate .js file this file will have some workouts in them. These will be pulled out and then used
+ *  "indoorWorkouts" :[{
+ *      "Jumping Jacks" : 40,
+ *      "Push Ups" : 100,
+ *      "Leg Lifts" : 50
+ *  }]
+ *
+ *  "gymWorkouts" :[{
+ *      "Pull Ups" : 40,
+ *      "Curls" : 40,
+ *      "Bench Press" : 50
+ *  }]
+ *
+ * Math:
+ * My Math will be the math of how many hours they have to work out until they have to be somewhere.
+ *
+ * Array:
+ * 
+ * My array method will be indexOf() as I search the array for a certain piece of information
+ * 
+ */
+
