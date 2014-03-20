dcb79705e5a6ee28dbf2104f44acf8220e7b7842
Dustin Riley <driley137@fullsail.edu>
2014-03-20 19:22:53 -0400

    Getting back in
    Some difficulty knowing where exactly to start.

---
+++ json.js
@@ -0,0 +1,16 @@
+var jsonWorkout = {
+    
+    "indoorWorkouts" :[{
+      "Jumping Jacks" : 40,
+      "Push Ups" : 100,
+      "Leg Lifts" : 50
+ }]
+
+  "gymWorkouts" :[{
+      "Pull Ups" : 40,
+     "Curls" : 40,
+      "Bench Press" : 50
+ }]
+  
+  
+} // END jsonWorkout
